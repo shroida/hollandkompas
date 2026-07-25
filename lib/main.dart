91d46acc8abf6e06eac3edd5a253c2761a7045e9
shroida <shroida012@gmail.com>
2026-07-25 13:15:31 +0300

    modify main

--- lib/main.dart
+++ lib/main.dart
@@ -3,20 +3,31 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hollandkompas/core/supabase_config.dart';
 
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
 
-  await dotenv.load(fileName: ".env");
+
+  await dotenv.load(
+    fileName: ".env",
+  );
+
 
   await SupabaseConfig.init();
 
+
   runApp(const MyApp());
+
 }
 
+
 class MyApp extends StatelessWidget {
+
   const MyApp({super.key});
+
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -25,5 +36,6 @@ class MyApp extends StatelessWidget {
         ),
       ),
     );
+
   }
 }
