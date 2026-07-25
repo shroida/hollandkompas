2ff0dbd611e91a904601db38c1c50db734687c38
shroida <shroida012@gmail.com>
2026-07-25 13:43:39 +0300

    remove env

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -1,11 +1,7 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 class Env {
-  static String get supabaseUrl {
-    return dotenv.env['SUPABASE_URL'] ?? '';
-  }
+  static const String supabaseUrl =
+      String.fromEnvironment('SUPABASE_URL');
 
-  static String get supabasePublishableKey {
-    return dotenv.env['SUPABASE_PUBLISHABLE_KEY'] ?? '';
-  }
+  static const String supabasePublishableKey =
+      String.fromEnvironment('SUPABASE_PUBLISHABLE_KEY');
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hollandkompas/core/supabase_config.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
-
-
-  await dotenv.load(
-    fileName: ".env",
-  );
-
 
   await SupabaseConfig.init();
 
-
   runApp(const MyApp());
-
 }
 
-
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -36,6 +22,5 @@ class MyApp extends StatelessWidget {
         ),
       ),
     );
-
   }
 }
