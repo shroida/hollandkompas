78aa10ba7ea255ccdced73675a9e192695743847
shroida <shroida012@gmail.com>
2026-07-26 12:44:27 +0300

    apply dir in app for arabic

--- lib/holland_kompas.dart
+++ lib/holland_kompas.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:hollandkompas/core/localization/app_locale.dart';
 import 'package:hollandkompas/core/router/app_router.dart';
 
-class HollandKompas extends StatelessWidget {
+class HollandKompas extends ConsumerWidget {
   const HollandKompas({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+    WidgetRef ref,
+  ) {
+    final locale = ref.watch(
+      appLocaleProvider,
+    );
+
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'HollandKompas',
       routerConfig: appRouter,
+      locale: locale,
+
+      builder: (context, child) {
+        return Directionality(
+          textDirection:
+              locale.languageCode == 'ar'
+                  ? TextDirection.rtl
+                  : TextDirection.ltr,
+          child: child!,
+        );
+      },
     );
   }
 }
