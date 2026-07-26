6c1d8e9d5852679b8fa32a57165fc77aff81589b
shroida <shroida012@gmail.com>
2026-07-26 12:36:53 +0300

    TabletHomeView

---
+++ lib/features/home/presentation/views/tablet_home_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TabletHomeView extends StatelessWidget {
+  const TabletHomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Tablet Home'),
+      ),
+    );
+  }
+}
