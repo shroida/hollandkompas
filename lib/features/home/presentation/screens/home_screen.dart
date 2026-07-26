eb9492e25ffd117f9f20ef659ed350be47d7a30c
shroida <shroida012@gmail.com>
2026-07-26 12:56:52 +0300

    LanguageSelector

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'package:hollandkompas/core/localization/app_locale.dart';
 import 'package:hollandkompas/core/responsive/responsive_builder.dart';
-
 import 'package:hollandkompas/features/home/presentation/views/desktop_home_view.dart';
 import 'package:hollandkompas/features/home/presentation/views/mobile_home_view.dart';
 import 'package:hollandkompas/features/home/presentation/views/tablet_home_view.dart';
+import 'package:hollandkompas/shared/widgets/language_selector.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
@@ -16,40 +14,15 @@ class HomeScreen extends ConsumerWidget {
     BuildContext context,
     WidgetRef ref,
   ) {
-    final locale = ref.watch(appLocaleProvider);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HollandKompas'),
-        actions: [
-          DropdownButton<String>(
-            value: locale.languageCode,
-            underline: const SizedBox(),
-            items: const [
-              DropdownMenuItem(
-                value: 'ar',
-                child: Text('🇪🇬 العربية'),
-              ),
-              DropdownMenuItem(
-                value: 'nl',
-                child: Text('🇳🇱 Nederlands'),
-              ),
-              DropdownMenuItem(
-                value: 'en',
-                child: Text('🇺🇸 English'),
-              ),
-            ],
-            onChanged: (value) {
-              if (value != null) {
-                ref
-                    .read(appLocaleProvider.notifier)
-                    .changeLanguage(value);
-              }
-            },
-          ),
-          const SizedBox(width: 16),
+     appBar: AppBar(
+      title: const Text('HollandKompas'),
+      actions: const [
+        LanguageSelector(),
+        SizedBox(width: 16),
         ],
-      ),
+),
       body: const ResponsiveBuilder(
         mobile: MobileHomeView(),
         tablet: TabletHomeView(),
