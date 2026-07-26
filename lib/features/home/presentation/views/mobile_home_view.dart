669dd34090cc282ae16b54e8424a434cac52d298
shroida <shroida012@gmail.com>
2026-07-26 12:36:38 +0300

    MobileHomeView

---
+++ lib/features/home/presentation/views/mobile_home_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MobileHomeView extends StatelessWidget {
+  const MobileHomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Mobile Home'),
+      ),
+    );
+  }
+}
