343d4bb14334246a2c355d6a1989ca65459ef202
shroida <shroida012@gmail.com>
2026-07-26 12:24:14 +0300

    HiveService init in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hollandkompas/core/config/supabase_config.dart';
+import 'package:hollandkompas/core/storage/hive_service.dart';
 import 'package:hollandkompas/holland_kompas.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await HiveService.init();
   await SupabaseConfig.init();
 
   runApp(
