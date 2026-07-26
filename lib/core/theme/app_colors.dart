45405c413b2f3d9a01aa1d36a0ce048c1efa7d2a
shroida <shroida012@gmail.com>
2026-07-26 12:58:42 +0300

    AppColors

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColors {
+  static const primary = Color(0xFF005BBB);
+  static const secondary = Color(0xFFFFD500);
+
+  static const background = Color(0xFFF8F9FA);
+
+  static const textPrimary = Color(0xFF212529);
+  static const textSecondary = Color(0xFF6C757D);
+
+  static const success = Color(0xFF28A745);
+  static const error = Color(0xFFDC3545);
+}
