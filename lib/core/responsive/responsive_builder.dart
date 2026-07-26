d9a47b914ac373ebdc91323d0afa00f533ea1d52
shroida <shroida012@gmail.com>
2026-07-26 12:33:16 +0300

    ResponsiveBuilder

---
+++ lib/core/responsive/responsive_builder.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'breakpoints.dart';
+
+class ResponsiveBuilder extends StatelessWidget {
+  final Widget mobile;
+  final Widget? tablet;
+  final Widget? desktop;
+
+  const ResponsiveBuilder({
+    super.key,
+    required this.mobile,
+    this.tablet,
+    this.desktop,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final width =
+        MediaQuery.of(context).size.width;
+
+    if (width >= Breakpoints.tablet) {
+      return desktop ?? mobile;
+    }
+
+    if (width >= Breakpoints.mobile) {
+      return tablet ?? mobile;
+    }
+
+    return mobile;
+  }
+}
