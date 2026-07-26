9ee7df32e932412df4e3d11046b459391d681e74
shroida <shroida012@gmail.com>
2026-07-26 12:35:27 +0300

    ResponsiveExtension helper

--- lib/core/responsive/responsive_extension.dart
+++ lib/core/responsive/responsive_extension.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
 
-import 'breakpoints.dart';
-
 extension ResponsiveExtension on BuildContext {
-  double get width => MediaQuery.of(this).size.width;
-  double get height => MediaQuery.of(this).size.height;
+  // Screen Size
+  Size get size => MediaQuery.sizeOf(this);
 
-  bool get isMobile =>
-      width < Breakpoints.mobile;
+  double get width => size.width;
+  double get height => size.height;
+
+  // Device Types
+  bool get isMobile => width < 600;
 
   bool get isTablet =>
-      width >= Breakpoints.mobile &&
-      width < Breakpoints.tablet;
+      width >= 600 && width < 1024;
 
-  bool get isDesktop =>
-      width >= Breakpoints.tablet;
+  bool get isDesktop => width >= 1024;
+
+  // Responsive Padding
+  double get pagePadding {
+    if (isDesktop) return 48;
+    if (isTablet) return 24;
+    return 16;
+  }
+
+  // Responsive Max Width
+  double get contentWidth {
+    if (isDesktop) return 1400;
+    if (isTablet) return 900;
+    return width;
+  }
+
+  // Responsive Font Scale
+  double get fontScale {
+    if (width >= 1440) return 1.3;
+    if (width >= 1024) return 1.15;
+    return 1.0;
+  }
 }
