352680fa897542ee37490d2f8d4269c0700a0aa3
shroida <shroida012@gmail.com>
2026-07-26 12:37:07 +0300

    DesktopHomeView

---
+++ lib/features/home/presentation/views/desktop_home_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DesktopHomeView extends StatelessWidget {
+  const DesktopHomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Desktop Home'),
+      ),
+    );
+  }
+}
