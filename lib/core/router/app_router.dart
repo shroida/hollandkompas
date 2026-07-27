9ad0a8e3a8d059cbb1c423a03f437bc550520ddb
shroida <shroida012@gmail.com>
2026-07-27 20:02:50 +0300

    solve splash in goRouter

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,14 +1,22 @@
 import 'package:go_router/go_router.dart';
-import 'package:hollandkompas/features/home/presentation/screens/home_screen.dart';
 
+import 'package:hollandkompas/features/home/presentation/screens/home_screen.dart';
+import 'package:hollandkompas/features/splash/presentation/pages/splash_page.dart';
 
 final appRouter = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) {
-        return const HomeScreen();
-      },
+      builder: (context, state) => SplashPage(
+        onDone: () {
+          context.go('/home');
+        },
+      ),
+    ),
+
+    GoRoute(
+      path: '/home',
+      builder: (context, state) => const HomeScreen(),
     ),
   ],
 );
