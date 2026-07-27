c3624a86ab67c1cdd425409df0b18f218022d8cb
shroida <shroida012@gmail.com>
2026-07-27 20:03:53 +0300

    adding Theme

--- lib/holland_kompas.dart
+++ lib/holland_kompas.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:hollandkompas/core/localization/app_locale.dart';
 import 'package:hollandkompas/core/router/app_router.dart';
+import 'package:hollandkompas/core/theme/app_theme.dart';
 
 class HollandKompas extends ConsumerWidget {
   const HollandKompas({super.key});
@@ -16,21 +17,26 @@ class HollandKompas extends ConsumerWidget {
       appLocaleProvider,
     );
 
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'HollandKompas',
-      routerConfig: appRouter,
-      locale: locale,
+   return MaterialApp.router(
+  debugShowCheckedModeBanner: false,
+  title: 'HollandKompas',
 
-      builder: (context, child) {
-        return Directionality(
-          textDirection:
-              locale.languageCode == 'ar'
-                  ? TextDirection.rtl
-                  : TextDirection.ltr,
-          child: child!,
-        );
-      },
+  routerConfig: appRouter,
+
+  theme: AppTheme.lightTheme,
+  darkTheme: AppTheme.darkTheme,
+
+  locale: locale,
+
+  builder: (context, child) {
+    return Directionality(
+      textDirection:
+          locale.languageCode == 'ar'
+              ? TextDirection.rtl
+              : TextDirection.ltr,
+      child: child!,
     );
+  },
+);
   }
 }
