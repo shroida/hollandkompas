264d11d25900d76354d07c201bf23773d646f6b9
shroida <shroida012@gmail.com>
2026-07-27 19:59:11 +0300

    create SplashNotifier

---
+++ lib/features/splash/providers/splash_provider.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final splashProvider =
+    StateNotifierProvider<SplashNotifier, double>((ref) {
+  return SplashNotifier();
+});
+
+class SplashNotifier extends StateNotifier<double> {
+  SplashNotifier() : super(0);
+
+  Timer? _timer;
+
+  void start(VoidCallback onDone) {
+    _timer = Timer.periodic(
+      const Duration(milliseconds: 60),
+      (timer) {
+        if (state >= 100) {
+          timer.cancel();
+
+          Future.delayed(
+            const Duration(milliseconds: 300),
+            onDone,
+          );
+        } else {
+          state += 2.5;
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+}
