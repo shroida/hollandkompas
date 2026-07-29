70d6a0c77c7ce0797ae68831abff9d6572c12d95
shroida <shroida012@gmail.com>
2026-07-29 21:42:14 +0300

    data slides

---
+++ lib/features/onboarding/data/onboarding_data.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+import 'package:hollandkompas/features/onboarding/models/onboarding_slide.dart';
+
+final slides = [
+  OnboardingSlide(
+    emoji: '🇳🇱',
+    titleAr: 'تعلم الهولندية بسهولة',
+    titleNl: 'Leer Nederlands gemakkelijk',
+    descAr:
+        'منهج شامل يأخذك من الصفر إلى مستوى B2 بخطوات منظمة ومدروسة.',
+    tags: [
+      'مفردات',
+      'قواعد',
+      'محادثة',
+      'قصص',
+    ],
+    gradient: [
+      Color(0xFFFF6B00),
+      Color(0xFFE55A00),
+    ],
+  ),
+];
