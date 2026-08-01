8fb7b25275f1e86aaf62380e7385a896ac39df0d
shroida <shroida012@gmail.com>
2026-08-01 08:55:16 +0300

    solve app router issue

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,5 +1,7 @@
 import 'package:go_router/go_router.dart';
+
 import 'package:hollandkompas/features/auth/presentation/pages/login_screen.dart';
+
 
 import 'package:hollandkompas/features/home/presentation/screens/home_screen.dart';
 import 'package:hollandkompas/features/onboarding/presentation/pages/onboarding_screen.dart';
@@ -10,23 +12,43 @@ final appRouter = GoRouter(
     GoRoute(
       path: '/',
       builder: (context, state) => SplashPage(
-        onDone: () {
-          context.go('/onboarding');
-        },
+        onDone: () => context.go('/onboarding'),
       ),
+    ),
+
+    GoRoute(
+      path: '/onboarding',
+      builder: (context, state) => const OnboardingScreen(),
     ),
 
     GoRoute(
       path: '/home',
       builder: (context, state) => const HomeScreen(),
     ),
-    GoRoute(
-      path: '/onboarding',
-      builder: (context, state) => const OnboardingScreen(),
-    ),
+
     GoRoute(
       path: '/login',
-      builder: (context, state) => const LoginScreen(),
+      builder: (context, state) => LoginScreen(
+        onLogin: () => context.go('/home'),
+        onRegister: () => context.go('/register'),
+        onForgot: () => context.go('/forgot-password'),
+      ),
     ),
+
+    // GoRoute(
+    //   path: '/register',
+    //   builder: (context, state) => RegisterScreen(
+    //     onLogin: () => context.go('/login'),
+    //     onBack: () => context.pop(),
+    //   ),
+    // ),
+
+    // GoRoute(
+    //   path: '/forgot-password',
+    //   builder: (context, state) => ForgotPasswordScreen(
+    //     onBack: () => context.pop(),
+    //     onLogin: () => context.go('/login'),
+    //   ),
+    // ),
   ],
 );
