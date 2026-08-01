fd2d11f91ac7d6fa7408d78443a30e6149521085
shroida <shroida012@gmail.com>
2026-08-01 09:56:47 +0300

    AuthState in riverpod

---
+++ lib/features/auth/presentation/providers/auth_state.dart
@@ -0,0 +1,25 @@
+import 'package:hollandkompas/features/auth/domain/entities/app_user.dart';
+
+class AuthState {
+  final bool isLoading;
+  final AppUser? user;
+  final String? error;
+
+  const AuthState({
+    this.isLoading = false,
+    this.user,
+    this.error,
+  });
+
+  AuthState copyWith({
+    bool? isLoading,
+    AppUser? user,
+    String? error,
+  }) {
+    return AuthState(
+      isLoading: isLoading ?? this.isLoading,
+      user: user ?? this.user,
+      error: error,
+    );
+  }
+}
