610d3dc22ca392b22b471f70bc4a7b30255917b8
shroida <shroida012@gmail.com>
2026-08-01 09:39:36 +0300

    AppUser create in domain

---
+++ lib/features/auth/domain/entities/app_user.dart
@@ -0,0 +1,15 @@
+class AppUser {
+  final String id;
+  final String email;
+  final String fullName;
+  final String level;
+  final String role;
+
+  const AppUser({
+    required this.id,
+    required this.email,
+    required this.fullName,
+    required this.level,
+    required this.role,
+  });
+}
