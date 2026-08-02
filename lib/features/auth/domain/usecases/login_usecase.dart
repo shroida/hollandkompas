99118113ef6f5405a7f5d4b354b2ac207e6dffac
shroida <shroida012@gmail.com>
2026-08-02 10:05:03 +0300

    LoginUseCase

---
+++ lib/features/auth/domain/usecases/login_usecase.dart
@@ -0,0 +1,18 @@
+import 'package:hollandkompas/features/auth/domain/entities/app_user.dart';
+import 'package:hollandkompas/features/auth/domain/repositories/auth_repository.dart';
+
+class LoginUseCase {
+  final AuthRepository repository;
+
+  LoginUseCase(this.repository);
+
+  Future<AppUser> call({
+    required String email,
+    required String password,
+  }) {
+    return repository.login(
+      email: email,
+      password: password,
+    );
+  }
+}
