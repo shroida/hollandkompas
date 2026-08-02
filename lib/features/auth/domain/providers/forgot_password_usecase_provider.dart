35525b2d9b17e14d6adfd33ed24273e201aeea59
shroida <shroida012@gmail.com>
2026-08-02 11:18:37 +0300

    forgotPasswordUseCaseProvider

---
+++ lib/features/auth/domain/providers/forgot_password_usecase_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../data/providers/auth_repository_provider.dart';
+import '../usecases/forgot_password_usecase.dart';
+
+final forgotPasswordUseCaseProvider =
+    Provider<ForgotPasswordUseCase>((ref) {
+  return ForgotPasswordUseCase(
+    ref.watch(authRepositoryProvider),
+  );
+});
