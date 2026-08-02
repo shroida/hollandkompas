073987a53be28ede721928733385428fcc0cfae4
shroida <shroida012@gmail.com>
2026-08-02 09:59:46 +0300

    solve error for publishableKey

--- lib/core/network/supabase_client.dart
+++ lib/core/network/supabase_client.dart
@@ -4,7 +4,7 @@ class SupabaseManager {
   static Future<void> init() async {
     await Supabase.initialize(
       url: 'https://luemujpnocopxmaomtyd.supabase.co',
-      anonKey: 'sb_publishable_AM0TsYrIr5uYjH4isEukDA_GPJ3ILP_',
+      publishableKey: 'sb_publishable_AM0TsYrIr5uYjH4isEukDA_GPJ3ILP_',
     );
   }
 }
