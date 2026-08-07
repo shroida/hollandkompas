b17ad37d7e0d93ed1648dfa8695bd2efaea2a6cb
shroida <shroida012@gmail.com>
2026-08-07 19:18:34 +0300

    refactor main remove debug

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,19 @@ import 'package:hollandkompas/core/router/app_router.dart';
 import 'package:hollandkompas/core/storage/hive_service.dart';
 import 'package:hollandkompas/holland_kompas.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await SupabaseManager.init();
-
-
   await HiveService.init();
 
- Supabase.instance.client.auth.onAuthStateChange.listen((data) {
-  debugPrint("EVENT: ${data.event}");
-  debugPrint("SESSION: ${data.session != null}");
+  Supabase.instance.client.auth.onAuthStateChange.listen((data) {
+    if (data.event == AuthChangeEvent.passwordRecovery) {
+      appRouter.go('/reset-password');
+    }
+  });
 
-Supabase.instance.client.auth.onAuthStateChange.listen((data) {
-  if (data.event == AuthChangeEvent.passwordRecovery) {
-    appRouter.go('/reset-password');
-  }
-});
-});
   runApp(
     const ProviderScope(
       child: HollandKompas(),
