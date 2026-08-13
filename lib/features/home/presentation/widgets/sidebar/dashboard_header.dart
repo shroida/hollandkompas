a323df75e6e20024c66b0a541e7f638b7173a741
shroida <shroida012@gmail.com>
2026-08-13 21:36:22 +0300

    DashboardHeader

---
+++ lib/features/home/presentation/widgets/sidebar/dashboard_header.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:hollandkompas/core/theme/app_colors.dart';
+
+class DashboardHeader extends StatelessWidget {
+  const DashboardHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Admin Dashboard",
+          style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+            fontWeight: FontWeight.bold,
+            color: AppColors.textForeground,
+          ),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          "Manage students, courses and platform statistics.",
+          style: Theme.of(
+            context,
+          ).textTheme.bodyMedium?.copyWith(color: AppColors.mutedForeground),
+        ),
+      ],
+    );
+  }
+}
