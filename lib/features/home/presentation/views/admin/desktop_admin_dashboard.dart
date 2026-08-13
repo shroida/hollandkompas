0c48ca9c70083e451dd02d5d2dd2ea4729470118
shroida <shroida012@gmail.com>
2026-08-13 21:24:12 +0300

    DesktopAdminDashboard

---
+++ lib/features/home/presentation/views/admin/desktop_admin_dashboard.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'mobile_admin_dashboard.dart';
+
+class DesktopAdminDashboard extends StatelessWidget {
+  const DesktopAdminDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MobileAdminDashboard();
+  }
+}
