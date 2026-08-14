91561bc6d2db9ffcf43101f50c1d5418f28cf501
shroida <shroida012@gmail.com>
2026-08-14 20:01:41 +0300

    RecentCourseModel

--- lib/features/home/data/datasource/admin_remote_data_source.dart
+++ lib/features/home/data/datasource/admin_remote_data_source.dart
@@ -1,13 +1,1 @@
-abstract interface class AdminRemoteDataSource {
-  Future<int> getStudentsCount();
 
-  Future<int> getCoursesCount();
-
-  Future<int> getLessonsCount();
-
-  Future<int> getEnrollmentsCount();
-
-  Future<List<RecentStudentModel>> getRecentStudents();
-
-  Future<List<RecentCourseModel>> getRecentCourses();
-}
---
+++ lib/features/home/data/models/recent_student_model.dart
@@ -0,0 +1,17 @@
+class RecentStudentModel {
+  final String id;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String level;
+  final DateTime createdAt;
+
+  RecentStudentModel({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.level,
+    required this.createdAt,
+  });
+}
