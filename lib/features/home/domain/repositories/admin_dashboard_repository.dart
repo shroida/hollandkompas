ed1370351820069b078307c7836216b45d1b721f
shroida <shroida012@gmail.com>
2026-08-14 20:08:00 +0300

    AdminDashboardRepository

---
+++ lib/features/home/domain/repositories/admin_dashboard_repository.dart
@@ -0,0 +1,11 @@
+import 'package:hollandkompas/features/home/domain/entities/dashboard_statistics.dart';
+import 'package:hollandkompas/features/home/domain/entities/recent_course.dart';
+import 'package:hollandkompas/features/home/domain/entities/recent_student.dart';
+
+abstract interface class AdminDashboardRepository {
+  Future<DashboardStatistics> getDashboardStatistics();
+
+  Future<List<RecentStudent>> getRecentStudents();
+
+  Future<List<RecentCourse>> getRecentCourses();
+}
