5e617816b3c6e6bcc1ed87251a99159434c8b569
shroida <shroida012@gmail.com>
2026-08-14 20:45:33 +0300

    StatisticsGrid

---
+++ lib/features/home/presentation/widgets/dashboard/statistics_grid.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../providers/admin_dashboard_providers.dart';
+import 'statistic_card.dart';
+
+class StatisticsGrid extends ConsumerWidget {
+  const StatisticsGrid({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final statistics = ref.watch(dashboardStatisticsProvider);
+
+    return statistics.when(
+      loading: () => const Center(child: CircularProgressIndicator()),
+
+      error: (e, _) => Text(e.toString()),
+
+      data: (stats) {
+        return GridView.count(
+          crossAxisCount: 2,
+
+          shrinkWrap: true,
+
+          physics: const NeverScrollableScrollPhysics(),
+
+          crossAxisSpacing: 16,
+
+          mainAxisSpacing: 16,
+
+          childAspectRatio: 2,
+
+          children: [
+            StatisticCard(
+              title: "Students",
+              value: stats.totalStudents.toString(),
+              icon: Icons.people,
+            ),
+
+            StatisticCard(
+              title: "Courses",
+              value: stats.totalCourses.toString(),
+              icon: Icons.menu_book,
+            ),
+
+            StatisticCard(
+              title: "Lessons",
+              value: stats.totalLessons.toString(),
+              icon: Icons.play_lesson,
+            ),
+
+            StatisticCard(
+              title: "Enrollments",
+              value: stats.totalEnrollments.toString(),
+              icon: Icons.school,
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
