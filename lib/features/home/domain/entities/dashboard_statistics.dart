d2cc790712d73b6fd7dd6ae8caeaee469cdfd330
shroida <shroida012@gmail.com>
2026-08-14 20:06:57 +0300

    DashboardStatistics

---
+++ lib/features/home/domain/entities/dashboard_statistics.dart
@@ -0,0 +1,13 @@
+class DashboardStatistics {
+  final int totalStudents;
+  final int totalCourses;
+  final int totalLessons;
+  final int totalEnrollments;
+
+  const DashboardStatistics({
+    required this.totalStudents,
+    required this.totalCourses,
+    required this.totalLessons,
+    required this.totalEnrollments,
+  });
+}
