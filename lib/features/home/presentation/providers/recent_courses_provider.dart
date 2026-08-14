fc5530cf62e446b648c8ce52dbb5c0421843bdf8
shroida <shroida012@gmail.com>
2026-08-14 20:32:44 +0300

    declare course and student providers

--- lib/features/home/presentation/providers/recent_courses_provider.dart
+++ lib/features/home/presentation/providers/recent_courses_provider.dart
@@ -2,11 +2,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'admin_dashboard_repository_provider.dart';
 
-import 'package:hollandkompas/features/home/domain/entities/recent_student.dart';
-import 'package:hollandkompas/features/home/domain/usecases/get_recent_students.dart';
+import 'package:hollandkompas/features/home/domain/entities/recent_course.dart';
+import 'package:hollandkompas/features/home/domain/usecases/get_recent_courses.dart';
 
-final recentStudentsProvider = FutureProvider<List<RecentStudent>>((ref) async {
-  final useCase = GetRecentStudents(ref.read(adminDashboardRepositoryProvider));
+final recentCoursesProvider = FutureProvider<List<RecentCourse>>((ref) async {
+  final useCase = GetRecentCourses(ref.read(adminDashboardRepositoryProvider));
 
   return useCase();
 });
