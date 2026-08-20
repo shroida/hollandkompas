ef12a92be44900113d5ee97fe10d4d6fbc307632
shroida <shroida012@gmail.com>
2026-08-20 21:32:40 +0300

    Course entity

---
+++ lib/features/home/domain/entities/course.dart
@@ -0,0 +1,23 @@
+class Course {
+  final String id;
+  final String title;
+  final String description;
+  final String level;
+  final String? imageUrl;
+  final bool isPublished;
+  final String? createdBy;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+
+  const Course({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.level,
+    required this.imageUrl,
+    required this.isPublished,
+    required this.createdBy,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+}
