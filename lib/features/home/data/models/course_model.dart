704114095404c0b2aa96dc9a968773dd44f27230
shroida <shroida012@gmail.com>
2026-08-20 21:33:35 +0300

    create CourseModel

---
+++ lib/features/home/data/models/course_model.dart
@@ -0,0 +1,27 @@
+class CourseModel extends Course {
+  const CourseModel({
+    required super.id,
+    required super.title,
+    required super.description,
+    required super.level,
+    required super.imageUrl,
+    required super.isPublished,
+    required super.createdBy,
+    required super.createdAt,
+    required super.updatedAt,
+  });
+
+  factory CourseModel.fromJson(Map<String, dynamic> json) {
+    return CourseModel(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      description: json['description'] as String,
+      level: json['level'] as String,
+      imageUrl: json['image_url'] as String?,
+      isPublished: json['is_published'] as bool? ?? false,
+      createdBy: json['created_by'] as String?,
+      createdAt: DateTime.parse(json['created_at'] as String),
+      updatedAt: DateTime.parse(json['updated_at'] as String),
+    );
+  }
+}
