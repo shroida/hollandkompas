65f3984e0cb961868b8629c31d2832c4a855bc67
shroida <shroida012@gmail.com>
2026-08-20 21:35:49 +0300

    impl for CourseRemoteDataSource

---
+++ lib/features/home/data/datasource/course_remote_data_source_impl.dart
@@ -0,0 +1,22 @@
+import 'package:hollandkompas/features/home/data/datasource/course_remote_data_source.dart';
+import 'package:hollandkompas/features/home/data/models/course_model.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class CourseRemoteDataSourceImpl implements CourseRemoteDataSource {
+  final SupabaseClient supabase;
+
+  CourseRemoteDataSourceImpl(this.supabase);
+
+  @override
+  Future<List<CourseModel>> getPublishedCourses() async {
+    final response = await supabase
+        .from('courses')
+        .select()
+        .eq('is_published', true)
+        .order('created_at', ascending: false);
+
+    return (response as List)
+        .map((json) => CourseModel.fromJson(Map<String, dynamic>.from(json)))
+        .toList();
+  }
+}
