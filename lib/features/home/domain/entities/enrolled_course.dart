3d43bde550c7a0b59e3d5433e894fda40a0bd132
shroida <shroida012@gmail.com>
2026-08-22 12:22:54 +0300

    EnrolledCourse

---
+++ lib/features/home/domain/entities/enrolled_course.dart
@@ -0,0 +1,21 @@
+import 'package:hollandkompas/features/home/domain/entities/course.dart';
+
+class EnrolledCourse {
+  final Course course;
+  final DateTime enrolledAt;
+  final int totalLessons;
+  final int completedLessons;
+
+  const EnrolledCourse({
+    required this.course,
+    required this.enrolledAt,
+    required this.totalLessons,
+    required this.completedLessons,
+  });
+
+  double get progress {
+    if (totalLessons == 0) return 0.0;
+
+    return completedLessons / totalLessons;
+  }
+}
