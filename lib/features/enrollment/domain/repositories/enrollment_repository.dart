aaeb9ae43e14276bd3ab1231b7a06ce6b907a07b
shroida <shroida012@gmail.com>
2026-08-28 20:29:04 +0300

    EnrollmentRepository

--- lib/features/enrollment/domain/repositories/enrollment_repository.dart
+++ lib/features/enrollment/domain/repositories/enrollment_repository.dart
@@ -1,8 +1,8 @@
+import 'package:hollandkompas/features/enrollment/domain/entities/coupon.dart';
 import 'package:hollandkompas/features/enrollment/domain/entities/enrolled_course.dart';
-
-import '../entities/coupon.dart';
 
 abstract class EnrollmentRepository {
   Future<Coupon?> getCoupon(String code);
+
   Future<List<EnrolledCourse>> getStudentEnrollments(String studentId);
 }
