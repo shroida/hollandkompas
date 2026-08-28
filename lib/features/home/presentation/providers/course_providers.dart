3ba3f5b95d9982cffe5b7bbbab9621c58c22608d
shroida <shroida012@gmail.com>
2026-08-28 22:06:17 +0300

    CourseRepositoryImpl

--- lib/features/home/presentation/providers/course_providers.dart
+++ lib/features/home/presentation/providers/course_providers.dart
@@ -2,7 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hollandkompas/core/network/app_exception.dart';
 import 'package:hollandkompas/features/courses/data/datasource/course_remote_data_source.dart';
 import 'package:hollandkompas/features/courses/data/datasource/course_remote_data_source_impl.dart';
-import 'package:hollandkompas/features/home/data/repositories/course_repository_impl.dart';
+import 'package:hollandkompas/features/courses/data/repositories/course_repository_impl.dart';
 import 'package:hollandkompas/features/home/domain/repositories/course_repository.dart';
 
 final courseRemoteDataSourceProvider = Provider<CourseRemoteDataSource>((ref) {
