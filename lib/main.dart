e8e3512510d9173d1ba73faebb73c3704e32c2fa
shroida <shroida012@gmail.com>
2026-09-08 11:54:55 +0300

    refactor main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hollandkompas/core/network/supabase_client.dart';
@@ -6,21 +8,29 @@ import 'package:hollandkompas/core/storage/hive_service.dart';
 import 'package:hollandkompas/holland_kompas.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await SupabaseManager.init();
-  await HiveService.init();
+  await _initializeApp();
 
-  Supabase.instance.client.auth.onAuthStateChange.listen((data) {
-    if (data.event == AuthChangeEvent.passwordRecovery) {
-      appRouter.go('/reset-password');
-    }
-  });
+  runApp(const ProviderScope(child: HollandKompas()));
+}
 
-  runApp(
-    const ProviderScope(
-      child: HollandKompas(),
-    ),
+Future<void> _initializeApp() async {
+  await Future.wait([SupabaseManager.init(), HiveService.init()]);
+
+  _listenToAuthChanges();
+}
+
+void _listenToAuthChanges() {
+  Supabase.instance.client.auth.onAuthStateChange.listen(
+    (data) {
+      if (data.event == AuthChangeEvent.passwordRecovery) {
+        appRouter.go('/reset-password');
+      }
+    },
+    onError: (error, stackTrace) {
+      debugPrint('Auth state error: $error');
+    },
   );
 }
