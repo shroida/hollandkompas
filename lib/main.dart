19e9baae9ceb695861c6c3dd0c2ad8c1ae81c7c8
shroida <shroida012@gmail.com>
2026-09-15 13:57:18 +0300

    main file refactored

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hollandkompas/core/network/supabase_client.dart';
-import 'package:hollandkompas/core/router/app_router.dart';
 import 'package:hollandkompas/core/storage/hive_service.dart';
 import 'package:hollandkompas/holland_kompas.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,15 +13,5 @@ Future<void> main() async {
 }
 
 Future<void> _initializeApp() async {
-  await Future.wait([SupabaseManager.init(), HiveService.init()]);
-
-  _listenToAuthChanges();
-}
-
-void _listenToAuthChanges() {
-  Supabase.instance.client.auth.onAuthStateChange.listen((data) {
-    if (data.event == AuthChangeEvent.passwordRecovery) {
-      appRouter.go('/reset-password');
-    }
-  }, onError: (error, stackTrace) {});
+  await Future.wait<void>([SupabaseManager.init(), HiveService.init()]);
 }
